53c3e079523f00ad87677cec253bfe2c63b4102a
saishyam234 <saishyam234@gmail.com>
2025-02-25 07:43:22 +0530

    upload the code

---
+++ src/Map.js
@@ -0,0 +1,35 @@
+// Map.js 
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix marker icon issue in Leaflet
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
+
+const customIcon = new L.Icon({
+  iconUrl: markerIconPng,
+  shadowUrl: markerShadowPng,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+const Map = ({ lat, lon }) => {
+  return (
+    <MapContainer center={[lat, lon]} zoom={13} style={{ height: "400px", width: "800px" }}>
+      {/* OpenStreetMap Tile Layer */}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      
+      {/* Marker at User's Location */}
+      <Marker position={[lat, lon]} icon={customIcon}>
+        <Popup>Your Location</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
